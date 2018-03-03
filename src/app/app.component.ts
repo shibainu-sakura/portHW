ca837b92bdb007d211d1b93f0077b56887ee536f
Tucker <takahirosuzuki.m010@gmail.com>
2018-03-03 19:10:51 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // templateUrl: './app.component.html',
+  template: 'Welcome to Port Hello World',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
